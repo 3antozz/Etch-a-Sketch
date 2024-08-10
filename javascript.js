79bd6fc76e36f61af47026f4ebad6ef6e8868b33
3antozz <106344848+3antozz@users.noreply.github.com>
2024-08-10 20:07:36 +0100

    complete the function that creates the grid

--- javascript.js
+++ javascript.js
@@ -1,6 +1,20 @@
-const squaresContainer = document.querySelector(".square-container");
-console.log(squaresContainer);
-const oneSquare = document.createElement("span");
-console.log(oneSquare);
-oneSquare.setAttribute("style", "height: 20px; width: 20px; border-style: solid; border-color: black");
-squaresContainer.appendChild(oneSquare);
+const gridContainer = document.querySelector(".grid-container");
+const body = document.querySelector("body");
+
+
+
+createGrid(100);
+function createGrid (number) {
+    let rowDiv;
+    for (let n=1; n <= number; n++) {
+        rowDiv = document.createElement("div");
+        rowDiv.setAttribute("style", "display: flex; flex: 1 1 auto; justify-content: center; box-sizing: border-box");
+        for (let i=1; i <= number; i++){
+            let newSquare = document.createElement("span");
+            newSquare.setAttribute("style", "box-sizing: border-box; display: flex; flex: 1 1 auto; border-color: black; border-style: solid; border-width: 0.5px");
+            rowDiv.appendChild(newSquare);
+            gridContainer.appendChild(rowDiv);
+        }
+    };
+
+}
